cd96571d023c48e5a28500035b143b58ae6ce120
Daru <radies@rambler.ru>
2026-01-21 13:31:51 +0300

    init

---
+++ telegram-service/src/main/java/ru/darujo/TelegramApp.java
@@ -0,0 +1,14 @@
+package ru.darujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class TelegramApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramApp.class, args);
+    }
+
+}
